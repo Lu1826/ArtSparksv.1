a08d6a3421db742f1132cbb4ab0b008b00bc9074
Lu1826 <lucianamariem2010@gmail.com>
2026-06-11 18:09:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,49 @@
 import streamlit as st
+import random
 
 st.title("🎨 ArtSpark")
-st.write("Mi proyecto está en construcción.")
+st.subheader("Generador creativo de ideas para artistas")
+
+personajes = [
+    "astronauta",
+    "pirata",
+    "chef",
+    "detective",
+    "mago"
+]
+
+emociones = [
+    "feliz",
+    "triste",
+    "enojado",
+    "curioso",
+    "sorprendido"
+]
+
+lugares = [
+    "una ciudad futurista",
+    "un castillo abandonado",
+    "el espacio",
+    "una isla misteriosa",
+    "un bosque mágico"
+]
+
+rarezas = [
+    "🟢 Común",
+    "🔮 Rara",
+    "✨ Épica",
+    "🏆 Legendaria"
+]
+
+if st.button("Generar Idea"):
+    
+    personaje = random.choice(personajes)
+    emoción = random.choice(emociones)
+    lugar = random.choice(lugares)
+    rareza = random.choice(rarezas)
+
+    st.success(
+        f"Dibuja un {personaje} {emocion} en {lugar}"
+    )
+
+    st.markdown(f"### {rareza}")

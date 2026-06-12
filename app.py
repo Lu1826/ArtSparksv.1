c9b0bc9cdc15c3ad03c376ec662dfeb12fa25d8e
Lu1826 <lucianamariem2010@gmail.com>
2026-06-11 20:05:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -94,6 +94,16 @@ with st.sidebar:
     if st.button("⭐ Ver Favoritos"):
 
         st.session_state.mostrar_favoritos = True
+            if len(st.session_state.favoritos) == 0:
+                st.info("No hay favoritos todavía")
+
+            else:
+                st.subheader("📚 Mis Favoritos")
+                for i, favorito in enumerate(
+                    st.session_state.favoritos,
+                    start=1
+                ):
+                    st.text(f"{i}. {favorito}")
 
 # CALCULADORA RAREZA
 def calcular_rareza(elementos):
@@ -468,20 +478,6 @@ with colA:
             st.success("Idea guardada en favoritos")
         else:
             st.warning("Primero genera una idea")
-with colB:
-    if st.button("📚 Ver Favoritos"):
-
-        if len(st.session_state.favoritos) == 0:
-            st.info("No hay favoritos todavía")
-
-        else:
-            st.subheader("📚 Mis Favoritos")
-
-            for i, favorito in enumerate(
-                st.session_state.favoritos,
-                start=1
-            ):
-                st.text(f"{i}. {favorito}")
 
 # BOTON DEAFIO DEL DIA
 

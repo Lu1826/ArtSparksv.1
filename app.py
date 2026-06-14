e29d3eea25ae1c2416aded6fc2e70a00af35361f
Lu1826 <lucianamariem2010@gmail.com>
2026-06-14 13:18:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,22 +6,24 @@ import os
 import base64
 
 #MUSIC
-def autoplay_audio(ruta_audio):
-    with open(ruta_audio, "rb") as f:
+
+def autoplay_audio(file_path):
+    with open(file_path, "rb") as f:
         data = f.read()
 
     b64 = base64.b64encode(data).decode()
 
     st.markdown(
         f"""
-        <audio controls autoplay loop style="display:none;">
+        <audio autoplay loop>
             <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
         </audio>
         """,
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
     )
 
 autoplay_audio("assets/music.mp3")
+
 #BARRA LATERAL DE FONDO
 
 def get_base64(imagen):

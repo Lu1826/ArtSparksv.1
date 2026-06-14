08ea033e9a9c2107ff836bbe55865e42de26d31a
Lu1826 <lucianamariem2010@gmail.com>
2026-06-13 22:56:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ datos = cargar_datos()
 with st.sidebar:
     st.markdown(f"""
 
-    <div style="height:100px;"></div>
+    <div style="height:150px;"></div>
 
     <div style="
         text-align:center;

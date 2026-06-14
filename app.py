2c29ee60ade18a010483eb46999cab493cedca1f
Lu1826 <lucianamariem2010@gmail.com>
2026-06-14 12:33:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -335,7 +335,7 @@ def generar_desafio_dia():
 
     return (
     f"Dibuja una persona {accion.lower()} "
-    f"sosteniendo {objeto.lower()} "
+    f"sosteniendo un{objeto.lower()} "
     f"acompañado de un {animal.lower()}, "
     f"con estilo {estilo} "
     f"y como color principal el {color}."
@@ -418,7 +418,7 @@ with st.sidebar:
     if st.button("⭐ VIEW FAVORITES"):
         st.session_state.mostrar_favoritos = not st.session_state.mostrar_favoritos
 
-    st.markdown("<div style='height:180px'></div>", unsafe_allow_html=True)
+    st.markdown("<div style='height:150px'></div>", unsafe_allow_html=True)
 # CHALLENGE OF THE DAY
     st.markdown(f"""
         <div style="
